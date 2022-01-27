2a03af5a9b44c8f834ce7b0994ca6dbcfa2e9cac
inthecatsdreams <inthecatsdreams@vk.com>
2022-01-27 14:00:57 +0100

    first commit

---
+++ app/src/main/java/com/inthecatsdreams/ouoijio/MainActivity.java
@@ -0,0 +1,50 @@
+package com.inthecatsdreams.ouoijio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    Context context = this;
+    MediaPlayer mp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mp = MediaPlayer.create(context, R.raw.fart);
+        final Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                        mp.stop();
+                        mp.release();
+                        mp = MediaPlayer.create(context, R.raw.fart);
+                        mp.start();
+                } catch(Exception e) { e.printStackTrace(); }
+            }
+        });
+        final Button niggerB = (Button) findViewById(R.id.button2);
+        niggerB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    mp.release();
+                    mp = MediaPlayer.create(context, R.raw.nigger);
+                    mp.start();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
